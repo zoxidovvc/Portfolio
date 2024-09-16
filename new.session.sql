13bc6f786550b972100aea601f41054a748b8c3c
zoxidovvc <sobitoff09@gmail.com>
2024-09-16 17:43:58 +0500

    first

---
+++ new.session.sql
@@ -0,0 +1,10 @@
+-- select * FROM apps_portfolio;
+
+-- SELECT text FROM apps_blog;
+-- SELECT category, title, text FROM apps_blog;
+
+
+-- INSERT INTO apps_blog (titlee) VALUES (yangii);
+-- INSERT INTO apps_blog title VALUES yangi;
+
+-- DROP TABLE apps_blog, apps_blogsingle, apps_comment, apps_opinion, apps_portfolio, apps_service, apps_skills, apps_statistic, apps_user, auth_group, auth_group_permissions, auth_permission, auth_user, auth_user_groups, auth_user_user_permissions, django_admin_log, django_content_type, django_migrations, django_session
